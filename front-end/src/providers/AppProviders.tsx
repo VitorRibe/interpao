fa2be0a284a59dd425b09b7891c722d8335d5682
T1T1R3 <thiago.souza091202@gmail.com>
2026-04-24 18:53:16 -0300

    initial structure of frontend

---
+++ front-end/src/providers/AppProviders.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
+import theme from '../theme';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
+
+interface AppProvidersProps {
+  children: ReactNode;
+}
+
+const AppProviders: React.FC<AppProvidersProps> = ({ children }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          {children}
+        </BrowserRouter>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default AppProviders;
