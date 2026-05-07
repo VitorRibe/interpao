9c5bcf400c99c42182eb2a81843e76b84994616b
T1T1R3 <thiago.souza091202@gmail.com>
2026-05-07 19:33:10 -0300

    fixed uuid in id of tables

--- back-end/app/models/session.py
+++ back-end/app/models/session.py
@@ -1,12 +1,13 @@
+import uuid
 from datetime import datetime, timezone
-from sqlalchemy import ForeignKey, String, DateTime
+from sqlalchemy import ForeignKey, UUID, DateTime, text
 from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
 
 class Session(Base):
     __tablename__ = "session"
     
-    id: Mapped[str] = mapped_column(String, primary_key=True) # session_id
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), index=True)
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4, server_default=text("gen_random_uuid()"))
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.id"), index=True)
     expires_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
